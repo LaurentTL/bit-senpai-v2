713c8b3e36b2558fe43c99dec53d7f3dd2037305
Laurent Treuil <laurent.treuil@gmail.com>
2022-09-14 09:04:23 +0200

    fix: logo head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
-            rel="stylesheet"
-          />
+          <link rel="icon" href="/static/images/logo.svg" />
         </Head>
         <body>
           <Main />
