403b19ec66f7c49e7ec4862893a3831ce913a74d
laurent treuil <laurent.treuil@gmail.com>
2023-02-21 14:47:15 +0100

    feat: logo link redirect to /

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,22 +1,23 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import logoBitSenpai from '../../public/static/images/logo.svg';
 
 export interface ILogo {}
 
 const Logo: React.FC<ILogo> = () => {
-  return (
-    <div className="flex gap-3 items-center h-100">
-      <div className="relative h-7 w-7">
-        <Image
-          src={logoBitSenpai}
-          fill
-          style={{ objectFit: 'contain' }}
-          alt="logo bitsenpai"
-        />
-      </div>
-      <p className="font-bold text-xl text-white">BitSenpai</p>
-    </div>
-  );
+    return (
+        <Link href={'/'} passHref className="flex gap-3 items-center h-100">
+            <div className="relative h-7 w-7">
+                <Image
+                    src={logoBitSenpai}
+                    fill
+                    style={{ objectFit: 'contain' }}
+                    alt="logo bitsenpai"
+                />
+            </div>
+            <p className="font-bold text-xl text-white">BitSenpai</p>
+        </Link>
+    );
 };
 
 export default Logo;
