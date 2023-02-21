880d7bbbb95a6e5595bb07cf49b1b244004fde0a
laurent treuil <laurent.treuil@gmail.com>
2023-02-21 14:33:05 +0100

    feat: toaster added apptsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,26 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from '../state/auth/AuthContext';
 import './globals.css';
 import { NextPageWithLayout } from './page';
 
 interface AppPropsWithLayout extends AppProps {
-  Component: NextPageWithLayout;
-  session: any;
+    Component: NextPageWithLayout;
+    session: any;
 }
 
 function MyApp({ Component, session, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout || ((page) => page);
+    const getLayout = Component.getLayout || ((page) => page);
 
-  return (
-    <SessionProvider session={session}>
-      <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>;
-    </SessionProvider>
-  );
+    return (
+        <SessionProvider session={session}>
+            <AuthProvider>
+                {getLayout(<Component {...pageProps} />)}
+            </AuthProvider>
+            <Toaster />
+        </SessionProvider>
+    );
 }
 
 export default MyApp;
