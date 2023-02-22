21374e50dd3b4554cb6412e2fc6423e4594e378a
laurent treuil <laurent.treuil@gmail.com>
2023-02-22 16:30:03 +0100

    build: packages update

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,62 +6,62 @@ import '../pages/globals.css';
 import { AuthProvider } from '../state/auth/AuthContext';
 
 const BREAKPOINTS_INT = {
-  xs: 375,
-  sm: 600,
-  md: 900,
-  lg: 1200,
-  xl: 1536,
+    xs: 375,
+    sm: 600,
+    md: 900,
+    lg: 1200,
+    xl: 1536,
 };
 
 const customViewports = Object.fromEntries(
-  Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
-    console.log(val);
-    return [
-      key,
-      {
-        name: key,
-        styles: {
-          width: `${val}px`,
-          height: `${(idx + 5) * 10}vh`,
-        },
-      },
-    ];
-  })
+    Object.entries(BREAKPOINTS_INT).map(([key, val], idx) => {
+        console.log(val);
+        return [
+            key,
+            {
+                name: key,
+                styles: {
+                    width: `${val}px`,
+                    height: `${(idx + 5) * 10}vh`,
+                },
+            },
+        ];
+    })
 );
 
 // Allow Storybook to handle Next's <Image> component
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => <OriginalNextImage {...props} unoptimized />,
+    configurable: true,
+    value: (props) => <OriginalNextImage {...props} unoptimized />,
 });
 
 export const decorators = [
-  (Story) => (
-    <AuthProvider>
-      <Story />
-    </AuthProvider>
-  ),
+    (Story) => (
+        <AuthProvider>
+            <Story />
+        </AuthProvider>
+    ),
 ];
 
 export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
     },
-  },
-  darkMode: {
-    // Override the default dark theme
-    dark: { ...themes.dark, appBg: 'black' },
-    // Override the default light theme
-    light: { ...themes.normal, appBg: 'red' },
-  },
-  viewport: { viewports: customViewports },
-  layout: 'fullscreen',
-  nextRouter: {
-    Provider: RouterContext.Provider,
-  },
+    darkMode: {
+        // Override the default dark theme
+        dark: { ...themes.dark, appBg: 'black' },
+        // Override the default light theme
+        light: { ...themes.normal, appBg: 'red' },
+    },
+    viewport: { viewports: customViewports },
+    layout: 'fullscreen',
+    nextRouter: {
+        Provider: RouterContext.Provider,
+    },
 };
