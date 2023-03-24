06a0749b0965140f608f021436155726b7214483
laurent treuil <laurent.treuil@gmail.com>
2023-03-24 12:01:13 +0100

    fix: add util to maintain polyfill

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    domains: ['assets.coingecko.com'],
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    resolve: {
+        fallback: {
+            util: require.resolve('util/'),
+        },
+    },
+    images: {
+        dangerouslyAllowSVG: true,
+        contentSecurityPolicy:
+            "default-src 'self'; script-src 'none'; sandbox;",
+        domains: ['assets.coingecko.com'],
+    },
 };
 
 module.exports = nextConfig;
