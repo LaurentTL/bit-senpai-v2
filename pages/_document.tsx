067d474021eedc044ad902900b760135512bd394
laurent treuil <laurent.treuil@gmail.com>
2023-03-30 15:03:55 +0200

    fix: gold logo favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,24 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link rel="icon" href="/static/images/logo.svg" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="icon" href="/static/logos/bs-logo-gold.svg" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
 
 export default MyDocument;
