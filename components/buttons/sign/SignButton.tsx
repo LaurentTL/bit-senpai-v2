36571f8b5518be4f2f077021852c1f6a2f74ba96
laurent treuil <laurent.treuil@gmail.com>
2023-04-05 12:37:34 +0200

    fix: sign up page link

--- components/buttons/sign/SignButton.tsx
+++ components/buttons/sign/SignButton.tsx
@@ -5,7 +5,7 @@ export interface ISignButton {}
 const SignButton: React.FC<ISignButton> = () => {
     return (
         <Link
-            href="/signup"
+            href="/sign-up"
             className="rounded-full w-24 py-1.5 block text-center from-blue-500 via-purple-600 to-pink-600 bg-gradient-20 text-white"
         >
             Sign up
